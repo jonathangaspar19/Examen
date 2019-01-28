d8ffd5d868d0569bfce45a604a2856a0dcd5aa12
jonathangaspar19 <35802510+jonathangaspar19@users.noreply.github.com>
2019-01-29 00:17:25 +0100

    Jonathan Gaspar #2
    Correccion: Me equivoqué de proyecto, le faltaban cosas

---
+++ Examen-master/src/main/java/es/salesianos/model/Actor.java
@@ -0,0 +1,13 @@
+package es.salesianos.model;
+
+public class Actor extends Director{
+	private Integer year;
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+}
---
+++ Examen-master/src/main/java/es/salesianos/model/Director.java
@@ -0,0 +1,13 @@
+package es.salesianos.model;
+
+public class Director extends BaseEntity{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
